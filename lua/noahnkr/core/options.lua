7a17dd2716d4e556f14a4e1125d75dd5ab431fe7
Noah Roberts <noahkrob97@gmail.com>
2024-01-25 12:08:34 -0600

    Updated terminal plugin

--- lua/noahnkr/core/options.lua
+++ lua/noahnkr/core/options.lua
@@ -2,7 +2,7 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
-
+opt.cursorline = true
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
